4d828e45ed0b67e3c53ec08020b87b947c3d9edd
blossom <14jj@naver.com>
2025-05-07 09:37:10 +0900

    Initial commit

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,7 +1,8 @@
 // src/services/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ufejrqckpcdszshvwjds.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmZWpycWNrcGNkc3pzaHZ3amRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MjQzMzgsImV4cCI6MjA2MjEwMDMzOH0.Xmzt9cF41qGqyx7c1tsSHSl5zEDf76SMJmpWS-0P-1Q'; // 여기에 너의 Supabase 프로젝트에서 발급받은 public anon key를 넣어야 해!
+// Vite 환경 변수에서 불러오기 (prefix VITE_ 필수)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
